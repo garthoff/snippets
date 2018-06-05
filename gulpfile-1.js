ca364afabdc9122ea3f97d7223d8dda1253ee58b
T. Garthoff <garthoff@users.noreply.github.com>
2018-06-05 20:33:10 +0200

    Create gulpfile-1.js
    This is a basic example of a gulpfile.js

---
+++ gulpfile-1.js
@@ -0,0 +1,24 @@
+/**
+
+Example of a basic gulp file
+
+**/
+
+var gulp = require( 'gulp' );
+var rename = require( 'gulp-rename' );
+var sass = require( 'gulp-sass' );
+
+var styleSRC = './src/scss/style.scss';
+var styleDST = './dist/css/';
+
+gulp.task('compile-style', function()
+{
+	gulp.src( styleSRC )
+		.pipe( sass({
+			errorLogToConsole: true,
+			outputStyle: 'compresses'
+		}) )
+		.on( 'error', console.error.bind( console ) )
+		.pipe( rename( { suffic: '.min' } ) )
+		.pipe( gulp.dest( styleDST ) );
+});
